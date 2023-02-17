45a7bec68d1950fd45d0fca5f4578dba79cdb0d2
Natalia Shuvaeva <glukomani@gmail.com>
2023-02-17 14:42:40 +0100

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableHighlight, Modal, TextInput } from 'react-native'
+
+export const Home = () => {
+  return (
+    <View>
+      <Text>{'test'}</Text>
+    </View>
+  )
+}
+
+export default Home
